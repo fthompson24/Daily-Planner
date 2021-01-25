bb844a0eb3817398f88f1ff11111d63a8b3141dc
fthompson24 <fraserjthompson@gmail.com>
2021-01-24 15:37:02 -1000

    task text and time saves to local storage

--- Develop/script.js
+++ Develop/script.js
@@ -3,3 +3,21 @@ $(document).ready(function() {
     $("#currentDay").text(moment().format('dddd MMM Do, YYYY'));
 });
 
+//time track - column change color depending on time
+function timeTracker() {
+
+}
+
+//add and save task using event listener
+$(document).ready(function() {
+    $(".saveBtn").on("click", function() {
+        //save time and text
+        var time=$(this).parent().attr("id");
+        var text=$(this).siblings(".description").val();
+
+        console.log(time, text);
+
+        //save to localStorage
+        localStorage.setItem(time, text);
+    })
+})
